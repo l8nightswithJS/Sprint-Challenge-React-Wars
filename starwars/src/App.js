a0b336bd663ca92394cb8ef1309ee66cc62451b3
eddie jimenez <ejmrtears89@gmail.com>
2019-09-30 23:50:00 -0700

    installed styled-components packages

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,9 +2,36 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Axios from 'axios';
 import CharacterCard from './CharacterCard';
+import './styles.css';
+import styled from 'styled-components';
 
 const App = () => {
   const [people, setPeople] = useState([])
+
+  const Wrapper = styled.section`
+  padding: 2em;
+  background: beige;
+`;
+
+  const Container = styled.div`
+  width: 100%;
+  height: auto;
+  border: 1px solid #c3c3c3;
+  display: -webkit-flex; /* Safari */
+  -webkit-flex-flow: row-reverse wrap; /* Safari 6.1+ */
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  `;
+
+  const People = styled.section`
+    width: 25%;
+    margin: 2%;
+    border: 2px black solid;
+    background-color: beige;
+    `; 
+
+  // const Header
   
 
   useEffect(() => {
@@ -20,19 +47,35 @@ const App = () => {
     }
     fetch()
 
-    },[])
+    },[people])
 
     return (
       <div className="App">
-        {people.map(person => {
-          return(
-            <CharacterCard name={person.name} year={person.birth_year} height={person.height} films={person.films} mass={person.mass} />
-          )
-        })}
+        <Wrapper><h1>StarWars Characters</h1></Wrapper>
+
+        <Container>
+
+          {people.map(person => {
+
+            return(
+              <People>
+
+                <CharacterCard 
+                  name={person.name} 
+                  year={person.birth_year} 
+                  height={person.height} 
+                  films={person.films} 
+                  mass={person.mass} />
+
+              </People>
+
+                )
+            })}
+
+        </Container>
+
         </div>
     )
-  
-  
   }
 
 export default App;
--- starwars/src/CharacterCard.js
+++ starwars/src/CharacterCard.js
@@ -1,22 +1,41 @@
 import React from 'react';
-
+import './styles.css';
+import styled from 'styled-components';
 
 //adding data function for grabbing data from api
+// const People = styled.section`
+//     width: 40%;
+//     margin: 2%;
+//     border: 2px black solid;
+//     padding: 1% 0 1% 0;
+//     background-color: beige;
+//     `; 
+
+const List = styled.ul`
+    list-style-type:none;
+    padding: 0;
+    `;
+
+const ListItem = styled.li`
+    margin: 2% 0 2% 0;
+    font-size: 1.62rem;
+    `;
+
+
 
 export default function CharacterCard(props) {
     return(
-        <section className="card">
-            <h1 className="char-name">{props.name}</h1>
-
+        <div>    
+            <h2 className="char-name">{props.name}</h2>
+            
             <div className="char-info">
-                <ul className="list">
-                    <li>{"Year Born: " + props.year}</li>
-                    <li>{"Height: " + props.height}</li>
-                    <li><a href={props.films}>{"Films I've been in."}</a></li>
-                    <li>{"Mass: " + props.mass}</li>
-                </ul>
+                <List>
+                    <ListItem>{"Year Born: " + props.year}</ListItem>
+                    <ListItem>{"Height: " + props.height}</ListItem>
+                    <List>{"Mass: " + props.mass}</List>
+                </List>
             </div>
-        </section>
+        </div>        
     )
 
 
